bf2b02f9a207ac13c49c8e3e3ee9f1e5df7c1540
An Hai Tran <64510590+an-hai-tran@users.noreply.github.com>
2023-04-09 21:14:02 -0400

    Add submit button

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ from transformers import pipeline
 #title
 st.title("Sentiment analysis of text")
 
-sentiment_pipeline = pipeline("sentiment-analysis")
-data = "I hate you"
-st.write(sentiment_pipeline(data))
+#substitle
+st.markdown("## We will analyze the sentence `I hate you` using a pretrained model from Hugging Face")
+
+if st.button("Submit"):
+    sentiment_pipeline = pipeline("sentiment-analysis")
+    data = "I hate you"
+    result = sentiment_pipeline(data)
+    st.write(result)
